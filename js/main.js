04cd782236fef31e4ed77e23f3d9a5101e685f6f
Michel Motta da Silva <michelmottadasilva@gmail.com>
2019-02-14 16:51:33 -0300

    adicionado loading

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
+Vue.component("loading-div", {
+    template: "#loading"
+});
+
 var app = new Vue({
     el: "#app",
+    mounted() {
+        console.log('HTML CARREGADO!!!');
+    },
     data: {
+        loadingStatus: false,
         pageTitle: "Desafio Jera - Star Wars"
+    },
+    methods: {
+
     }
 });
